d939b4820917057ac079068d82339be04521d6b9
Jason Reed <jcreed@gmail.com>
2017-12-16 18:58:18 -0500

    changes

--- midilog.js
+++ midilog.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const midi = require('midi');
-const input = new midi.input();
 const format = require('date-format');
-
-format.asString('hh:mm:ss.SSS', new Date());
+const spawn = require('child_process').spawn;
 
 const DEBUG = false;
-const GAP_SEC = 3;
+const GAP_SEC = 10;
+const LOW_NOTE = 36;
+const HIGH_NOTE = 90;
 
+const input = new midi.input();
 debug(count = input.getPortCount());
 for(let i = 0; i < count; i++)
   debug(i + ' ' + input.getPortName(i));
@@ -29,22 +30,34 @@ setInterval(heartbeat, GAP_SEC * 250);
 
 input.on('message', function(deltaTime, message) {
 
-  // a way of detecting whether the server is running by just
-  // manipulating the piano keyboard itself; if the intstrument is set
-  // to the default instrument, echo a 'hello world' note.
-  if (message[0] == 192 && message[1] == 0) {
-	 setTimeout(() => { output.sendMessage([144, 36, 32]); }, 0);
-	 setTimeout(() => { output.sendMessage([144, 36, 0]); }, 300);
-  }
-
-  const now = new Date();
-  if (state == undefined) {
-	 state = init_state(now, message);
+  if (message[0] == 192) {
+	 // a way of detecting whether the server is running by just
+	 // manipulating the piano keyboard itself; if the intstrument is set
+	 // to the default instrument, try to git push, and echo a 'hello world' note.
+	 if (message[1] == 0) {
+		if (state == undefined)
+		  play(HIGH_NOTE);
+		else {
+		  const shell = spawn(__dirname + '/push.sh', []);
+		  shell.stdout.on('data', data =>  console.log(data.toString()));
+		  shell.stderr.on('data', data =>  console.error(data.toString()));
+		  shell.on('exit', code => {
+			 console.log('child process exited with code ' + code.toString());
+			 play((code == 0) ? LOW_NOTE : HIGH_NOTE);
+		  });
+		}
+	 }
   }
   else {
-	 let delta = {midi_us: Math.round(deltaTime * 1e6), wall_ms: now - state.times.last};
-	 state.events.push({message, delta});
-	 state.times.last = now;
+	 const now = new Date();
+	 if (state == undefined) {
+		state = init_state(now, message);
+	 }
+	 else {
+		let delta = {midi_us: Math.round(deltaTime * 1e6), wall_ms: now - state.times.last};
+		state.events.push({message, delta});
+		state.times.last = now;
+	 }
   }
 });
 input.ignoreTypes(false, false, false);
@@ -62,9 +75,19 @@ function debug(...args) {
 // called every GAP_SEC / 4;
 function heartbeat() {
   if (state != undefined && ((new Date() - state.times.last) > GAP_SEC * 1000)) {
-	 // ship out a chunk
+	 ship_out();
+  }
+}
+
+function ship_out() {
+	 // ship out a chunk. Precondition: state should not be undefined if you're calling this.
 	 const filename = format.asString('yyyy-MM-dd.json', state.chunkStart);
 	 fs.appendFileSync(__dirname + '/log/' + filename, JSON.stringify({start: state.times.chunkStart, events: state.events}) + '\n');
+
 	 state = undefined;
-  }
+}
+
+function play(note) {
+  setTimeout(() => { output.sendMessage([144, note, 32]); }, 0);
+  setTimeout(() => { output.sendMessage([144, note, 0]); }, 300);
 }
