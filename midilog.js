084a0377d809641153a5b882d72fa9c355ba9466
Jason Reed <jcreed@gmail.com>
2017-12-16 19:06:36 -0500

    fix pushing behavior

--- midilog.js
+++ midilog.js
@@ -29,36 +29,41 @@ let state = undefined;
 setInterval(heartbeat, GAP_SEC * 250);
 
 input.on('message', function(deltaTime, message) {
+  debug(JSON.stringify({message, deltaTime}));
+  // ignore these messages
+  if (message[0] == 176 && message[1] == 0) return;
+  if (message[0] == 176 && message[1] == 32) return;
 
   if (message[0] == 192) {
 	 // a way of detecting whether the server is running by just
 	 // manipulating the piano keyboard itself; if the intstrument is set
 	 // to the default instrument, try to git push, and echo a 'hello world' note.
 	 if (message[1] == 0) {
-		if (state == undefined)
-		  play(HIGH_NOTE);
-		else {
-		  const shell = spawn(__dirname + '/push.sh', []);
-		  shell.stdout.on('data', data =>  console.log(data.toString()));
-		  shell.stderr.on('data', data =>  console.error(data.toString()));
-		  shell.on('exit', code => {
-			 console.log('child process exited with code ' + code.toString());
-			 play((code == 0) ? LOW_NOTE : HIGH_NOTE);
-		  });
+		if (state != undefined) {
+		  ship_out();
 		}
+		const shell = spawn(__dirname + '/push.sh', []);
+		shell.stdout.on('data', data =>  console.log(data.toString()));
+		shell.stderr.on('data', data =>  console.error(data.toString()));
+		shell.on('exit', code => {
+		  console.log('child process exited with code ' + code.toString());
+		  play((code == 0) ? LOW_NOTE : HIGH_NOTE);
+		});
 	 }
+	 return;
+  }
+
+  // Most stuff ends up here
+  const now = new Date();
+  if (state == undefined) {
+	 state = init_state(now, message);
   }
   else {
-	 const now = new Date();
-	 if (state == undefined) {
-		state = init_state(now, message);
-	 }
-	 else {
-		let delta = {midi_us: Math.round(deltaTime * 1e6), wall_ms: now - state.times.last};
-		state.events.push({message, delta});
-		state.times.last = now;
-	 }
+	 let delta = {midi_us: Math.round(deltaTime * 1e6), wall_ms: now - state.times.last};
+	 state.events.push({message, delta});
+	 state.times.last = now;
   }
+
 });
 input.ignoreTypes(false, false, false);
 process.stdin.on('data', function(x) { });
@@ -80,11 +85,11 @@ function heartbeat() {
 }
 
 function ship_out() {
-	 // ship out a chunk. Precondition: state should not be undefined if you're calling this.
-	 const filename = format.asString('yyyy-MM-dd.json', state.chunkStart);
-	 fs.appendFileSync(__dirname + '/log/' + filename, JSON.stringify({start: state.times.chunkStart, events: state.events}) + '\n');
+  // ship out a chunk. Precondition: state should not be undefined if you're calling this.
+  const filename = format.asString('yyyy-MM-dd.json', state.chunkStart);
+  fs.appendFileSync(__dirname + '/log/' + filename, JSON.stringify({start: state.times.chunkStart, events: state.events}) + '\n');
 
-	 state = undefined;
+  state = undefined;
 }
 
 function play(note) {
