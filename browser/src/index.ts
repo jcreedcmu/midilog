60425d7acb7f56b3fb2e1a0d13fcd38f17facac7
Jason Reed <jcreed@gmail.com>
2023-07-17 18:42:59 -0400

    Generate index dynamically

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -2,9 +2,6 @@ import * as fs from 'fs';
 import express from 'express';
 import * as path from 'path';
 import serveIndex from 'serve-index';
-import { generateIndex } from './generateIndex';
-
-generateIndex();
 
 const app = express();
 app.use(express.json());
@@ -14,7 +11,18 @@ app.use('/log', serveIndex(__dirname + '/../../log'));
 app.use('/log', express.static(__dirname + '/../../log'));
 
 app.get('/js/logger.js', (req, res) => { res.sendFile(path.resolve(__dirname, 'logger.js')) });
-app.get('/logIndex.json', (req, res) => { res.sendFile(path.resolve(__dirname, '../../logIndex.json')) });
+app.get('/logIndex.json', (req, res) => {
+
+  const metadata: { file: string, lines: number }[] = [];
+  const logDir = path.resolve(__dirname, '../../log');
+  const files = fs.readdirSync(logDir);
+  files.forEach(file => {
+    const lines = fs.readFileSync(path.join(logDir, file), 'utf8').split('\n').filter(x => x.length > 0).length
+    metadata.push({ file, lines });
+  });
+  res.json(metadata);
+
+});
 app.get('/', express.static(path.resolve(__dirname, '../public')));
 
 app.post('/api/save', (req, res) => {
