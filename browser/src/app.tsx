ad053eb95bf45d5f4b7c5e809cb4eea8b7f5e9d1
Jason Reed <jcreed@gmail.com>
2023-07-29 17:35:29 -0400

    Very rudimentary visualization

--- browser/src/app.tsx
+++ browser/src/app.tsx
@@ -1,7 +1,8 @@
 import { render } from 'preact';
-import { useState } from 'preact/hooks';
+import { useState, useEffect } from 'preact/hooks';
 import { allNotesOff, getText, unreachable } from './util';
 import { Index, Song, TimedSong, timedSong } from './types';
+import { CanvasInfo, CanvasRef, useCanvas } from './use-canvas';
 
 export type PlayCallback = (file: string, ix: number) => void;
 
@@ -15,13 +16,20 @@ export type AppProps = {
   output: WebMidi.MIDIOutput,
 };
 
+export type Playhead = {
+  eventIndex: number,
+  nowTime_ms: DOMHighResTimeStamp,
+};
+
+export type Playback = {
+  timeoutId: number,
+  playhead: Playhead,
+  startTime: DOMHighResTimeStamp,
+};
+
 export type AppState = {
-  song: TimedSong | undefined, playback: {
-    timeoutId: number,
-    eventIndex: number,
-    startTime: DOMHighResTimeStamp,
-    nowTime_ms: DOMHighResTimeStamp,
-  } | undefined
+  song: TimedSong | undefined,
+  playback: Playback | undefined,
 };
 
 export type Action =
@@ -33,7 +41,7 @@ export type Action =
 
 export type Dispatch = (action: Action) => void;
 
-function renderIndex(index: Index, dispatch: Dispatch): JSX.Element {
+function renderIndex(index: Index, dispatch: Dispatch, cref: CanvasRef): JSX.Element {
   const rows: JSX.Element[] = index.map(row => {
     const links: JSX.Element[] = [];
     for (let i = 0; i < row.lines; i++) {
@@ -47,33 +55,57 @@ function renderIndex(index: Index, dispatch: Dispatch): JSX.Element {
     return <tr><td>{row.file}</td> {links}</tr>;
   });
   rows.push(<tr><td><button onClick={() => dispatch({ t: 'panic' })}>Panic</button></td></tr>);
-  return <table>{rows}</table>;
+  return <div><table>{rows}</table>
+    <canvas
+      ref={cref} style={{ width: '100%', height: '200px', border: '1px solid black' }} />
+  </div>;
 }
 
-function playNextNote(state: AppState): Action {
-  if (state.playback === undefined)
-    return { t: 'none' };
-  if (state.song === undefined)
-    return { t: 'none' };
+function playNextNote(song: TimedSong, playhead: Playhead): Action {
+  // if (state.playback === undefined)
+  //   return { t: 'none' };
+  // if (state.song === undefined)
+  //   return { t: 'none' };
 
-  const { eventIndex, nowTime_ms } = state.playback;
+  const { eventIndex, nowTime_ms } = playhead;
 
-  const event = state.song.events[eventIndex];
-  const newIx = eventIndex + 1 < state.song.events.length ? eventIndex + 1 : undefined;
+  const event = song.events[eventIndex];
+  const newIx = eventIndex + 1 < song.events.length ? eventIndex + 1 : undefined;
   return { t: 'playNote', message: event.message, atTime_ms: event.time_ms, newIx };
+}
+
+function _renderMainCanvas(ci: CanvasInfo, state: AppState) {
+  ci.d.clearRect(0, 0, ci.size.x, ci.size.y);
+  if (state.song !== undefined) {
+    state.song.events.forEach(event => {
+      ci.d.fillRect(event.time_ms / 10, 10, 10, 10);
+    });
+  }
 }
 
 function App(props: AppProps): JSX.Element {
   const { index, output } = props;
   const [state, setState] = useState<AppState>({ playback: undefined, song: undefined });
+  const [cref, mc] = useCanvas(
+    state, _renderMainCanvas,
+    [state.playback, state.song],
+    () => { }
+  );
 
   const playCallback: PlayCallback = async (file, ix) => {
     const lines = (await getText(`/log/${file}`)).split('\n');
     const song = timedSong(JSON.parse(lines[ix]));
 
     const startTime = window.performance.now();
-    const timeoutId = window.setTimeout(() => dispatch(playNextNote(state)), 0);
-    setState(s => { s.song = song; s.playback = { timeoutId, eventIndex: 0, startTime, nowTime_ms: startTime }; return s; });
+    const playhead: Playhead = { eventIndex: 0, nowTime_ms: startTime };
+    const nextNoteAction = playNextNote(song, playhead);
+    console.log('set timeout', file, ix, nextNoteAction);
+    const timeoutId = window.setTimeout(() => dispatch(nextNoteAction), 0);
+
+    setState(s => {
+      console.log('set new state', file, ix);
+      return { song: song, playback: { timeoutId, playhead, startTime } };
+    });
 
 
   };
@@ -84,10 +116,13 @@ function App(props: AppProps): JSX.Element {
         playCallback(action.file, action.ix);
         break;
       case 'panic':
-        setState(s => {
-          s.playback = undefined;
-          return s;
-        });
+        if (state.playback !== undefined) {
+          clearTimeout(state.playback.timeoutId);
+          setState(s => {
+            s.playback = undefined;
+            return s;
+          });
+        }
         allNotesOff(output);
         break;
       case 'none':
@@ -96,14 +131,15 @@ function App(props: AppProps): JSX.Element {
         setState(s => {
           if (s.playback === undefined || s.song === undefined)
             return s;
+          const { song, playback } = s;
           console.log('sending', action.atTime_ms);
           output.send(action.message, s.playback.startTime + action.atTime_ms);
           if (action.newIx !== undefined) {
             const delay = Math.max(0,
               s.playback.startTime + s.song.events[action.newIx].time_ms
               - window.performance.now() - PLAYBACK_ANTICIPATION_MS);
-            s.playback.eventIndex = action.newIx;
-            setTimeout(() => dispatch(playNextNote(s)), delay);
+            s.playback.playhead.eventIndex = action.newIx;
+            setTimeout(() => dispatch(playNextNote(song, playback.playhead)), delay);
           }
           return s;
         });
@@ -112,7 +148,7 @@ function App(props: AppProps): JSX.Element {
         unreachable(action);
     }
   };
-  return renderIndex(index, dispatch);
+  return renderIndex(index, dispatch, cref);
 }
 
 export function init(props: AppProps) {
